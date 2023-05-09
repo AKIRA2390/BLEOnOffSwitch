4a262740636fc3c6b10dffab31c39c6c52ba4ce6
akira2390 <ando--akira@outlook.com>
2023-05-09 19:34:18 +0900

    init now

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+#include <NimBLEDevice.h>
+
+
+void setup() {
+    NimBLEDevice::init("NimBLE");
+    
+    NimBLEServer *pServer = NimBLEDevice::createServer();
+    NimBLEService *pService = pServer->createService("ABCD");
+    NimBLECharacteristic *pCharacteristic = pService->createCharacteristic("1234");
+    
+    pService->start();
+    pCharacteristic->setValue("Hello BLE");
+    
+    NimBLEAdvertising *pAdvertising = NimBLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID("ABCD"); 
+    pAdvertising->start(); 
+}
+
+void loop() {
+  
+}
